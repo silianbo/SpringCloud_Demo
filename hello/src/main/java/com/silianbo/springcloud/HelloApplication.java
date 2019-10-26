66cd8c6a6a5078a7ab53fa0db7990b305c69e6c3
silinabo <silianbo@qq.com>
2019-10-26 16:15:25 +0800

    HelloWorld 项目也进行注册
    Signed-off-by: silinabo <silianbo@qq.com>

--- hello/src/main/java/com/silianbo/springcloud/HelloApplication.java
+++ hello/src/main/java/com/silianbo/springcloud/HelloApplication.java
@@ -1,11 +1,13 @@
-package com.silianbo.springcloud.hello;
+package com.silianbo.springcloud;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HelloApplication {
     private static final Logger logger = LoggerFactory.getLogger(HelloApplication.class);
 
