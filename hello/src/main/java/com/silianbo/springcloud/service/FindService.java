44db092c4b3f621b2a1e3de374ccdfdfe45b8fc6
silinabo <silianbo@qq.com>
2019-10-26 20:17:51 +0800

    FeignClient fallback 的支持
    Signed-off-by: silinabo <silianbo@qq.com>

--- hello/src/main/java/com/silianbo/springcloud/service/FindService.java
+++ hello/src/main/java/com/silianbo/springcloud/service/FindService.java
@@ -1,16 +1,15 @@
 package com.silianbo.springcloud.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 1. FeignClient value 为接入的服务名称
  * 2. 服务接口的接口地址
  */
-@FeignClient(value = "hello")
+@FeignClient(value = "hello", fallback = FindServiceImpl.class)
 public interface FindService {
-    @RequestMapping(value = "/s", method = RequestMethod.GET)
+    @GetMapping(value = "/s")
     String helloService(@RequestParam("username") String username);
 }
