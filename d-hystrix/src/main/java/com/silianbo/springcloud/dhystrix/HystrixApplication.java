e537ad0fa6de9f789f2bfd1492b0cde305611de1
silinabo <silianbo@qq.com>
2019-10-28 18:16:37 +0800

    修改日志方式
    Signed-off-by: silinabo <silianbo@qq.com>

--- d-hystrix/src/main/java/com/silianbo/springcloud/dhystrix/HystrixApplication.java
+++ d-hystrix/src/main/java/com/silianbo/springcloud/dhystrix/HystrixApplication.java
@@ -1,13 +1,20 @@
 package com.silianbo.springcloud.dhystrix;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author bo
+ */
 @SpringBootApplication
 public class HystrixApplication {
+    private static final Logger logger = LoggerFactory.getLogger(HystrixApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(HystrixApplication.class, args);
+        logger.info("d-hystrix启动完成！！！");
     }
 
 }
